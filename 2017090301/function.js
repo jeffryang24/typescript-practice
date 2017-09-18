fdfe06bd27c0ca33930e6ee4b90fc572200811fa
Jeffry Angtoni <jeffryangtoni24@gmail.com>
2017-09-18 22:50:46 +0700

    -> new function.ts

--- 2017090301/function.js
+++ 2017090301/function.js
@@ -1,4 +1,12 @@
 var getSum = function (a, b) {
     return a + b;
 };
-document.write(getSum(2, 3).toString());
+document.write(getSum(2, 3).toString() + "<br />");
+var GetDiff = function (num1, num2, num3) {
+    if (num2 === void 0) { num2 = 2; }
+    if (typeof num3 !== 'undefined') {
+        return num1 - num2 - num3;
+    }
+    return num1 - num2;
+};
+document.write(GetDiff(2, 3).toString() + "<br />");
--- 2017090301/function.ts
+++ 2017090301/function.ts
@@ -2,4 +2,22 @@ var getSum = function(a:number, b:number):number{
     return a + b;
 }
 
-document.write(getSum(2,3).toString());
+document.write(getSum(2,3).toString() + "<br />");
+
+var GetDiff = function(num1:number, num2:number = 2, num3?:number){
+    if (typeof num3 !== 'undefined'){
+        return num1 - num2 - num3;
+    }
+
+    return num1 - num2;
+};
+
+document.write(GetDiff(2,3).toString() + "<br />");
+
+var sumAll = function(...nums: number[]): void{
+    var sum = nums.reduce((a,b) => a + b, 0);
+
+    document.write(sum + "<br />");
+};
+
+sumAll(1,2,3,4,5);
